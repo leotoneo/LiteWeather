33469ea622908e8f6813e78d528a31d97f636e5b
leo zhang <leo zhang>
2017-07-23 12:42:05 +0800

    add the member

--- app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java
+++ app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java
@@ -1,20 +1,46 @@
 package com.leotoneo.liteweather.liteweather;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import db.City;
+import db.County;
+import db.Province;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChooseAreaFragment extends Fragment {
 
-
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog  progressDialog;
+    private TextView textView;
+    private Button button;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<County> countyList;
+    private Province selectProvince;
+    private City selectCity;
+    private County selectCounty;
+    private int currentLevel;
     public ChooseAreaFragment() {
         // Required empty public constructor
     }
