3e2926d1ab425070d75d3fe4f9e16528c07104d8
leo zhang <leo zhang>
2017-07-23 12:26:44 +0800

    add fragment

---
+++ app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java
@@ -0,0 +1,35 @@
+package com.leotoneo.liteweather.liteweather;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChooseAreaFragment extends Fragment {
+
+
+    public ChooseAreaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
