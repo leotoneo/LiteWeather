b39d4a20f7317a1dff1a08f67abe8404e217c1a2
Neo Zhang <leotoneo@163.com>
2017-10-05 17:37:45 +0800

    modified:   app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java

--- app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java
+++ app/src/main/java/com/leotoneo/liteweather/liteweather/ChooseAreaFragment.java
@@ -8,17 +8,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.leotoneo.liteweather.liteweather.db.City;
 import com.leotoneo.liteweather.liteweather.db.County;
 import com.leotoneo.liteweather.liteweather.db.Province;
+import com.leotoneo.liteweather.liteweather.util.HttpUtil;
+import com.leotoneo.liteweather.liteweather.util.Utility;
 
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.RunnableFuture;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 /**
@@ -35,12 +47,33 @@ public class ChooseAreaFragment extends Fragment {
     private ListView listView;
     private ArrayAdapter<String> adapter;
     private List<String> dataList = new ArrayList<String>();
+    /**
+     * 省列表
+     */
     private List<Province> provinceList;
+    /**
+     * 市列表
+     */
     private List<City> cityList;
+    /**
+     * 县列表
+     */
     private List<County> countyList;
+    /**
+     * 选中省
+     */
     private Province selectProvince;
+    /**
+     * 选中市
+     */
     private City selectCity;
+    /**
+     * 选中县
+     */
     private County selectCounty;
+    /**
+     * 当前选中的级别
+     */
     private int currentLevel;
     public ChooseAreaFragment() {
         // Required empty public constructor
@@ -50,13 +83,138 @@ public class ChooseAreaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        textView = (TextView)view.findViewById(R.id.title_text);
+        button = (Button)view.findViewById(R.id.back_button);
+        listView = (ListView)view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if (currentLevel == LEVEL_PROVINCE) {
+                    selectProvince = provinceList.get(position);
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY) {
+                    selectCity = cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentLevel == LEVEL_COUNTY) {
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY) {
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
     }
+
+    /**
+     * 查询全国所有的省，优先去数据库查询，如果没有查询到再去服务器上查询
+     */
+    private void queryProvinces() {
+        textView.setText("中国");
+        button.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if (provinceList.size() > 0) {
+            dataList.clear();
+            for (Province province : provinceList) {
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        } else {
+            String addr = "http;//guolin.tech/api/china";
+            queryFromServer(addr,"province");
+        }
+    }
+
+    /**
+     * 根据传入的地址和类型从服务器上查询省市县数据
+     * @param addr
+     * @param type
+     */
+    private void queryFromServer(String addr, final String type) {
+        showProgressDilog();
+        HttpUtil.sendHttpRequest(addr, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String respondText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvinceRespond(respondText);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCityRespond(respondText,selectProvince.getId());
+                } else if ("county".equals(type)) {
+                    result = Utility.handleCountyRespond(respondText,selectCity.getId());
+                }
+                if (result) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)) {
+                                queryProvinces();
+                            } else if ("city".equals(type)) {
+                                queryCities();
+                            } else if ("county".equals(type)) {
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 查询所选择的省中的所有的市，优先去数据库查询，如果没有再去服务器上查询
+     */
+    private void queryCounties() {
+        textView.setText(selectProvince.getProvinceName());
+        button.setVisibility(View.VISIBLE);
+        cityList = DataSupport.where("province = ?",String.valueOf(selectProvince.getId())).find(City.class);
+    }
+
+    /**
+     * 查询所选择的市中的所有的县，优先去数据库查询，如果没有再去服务器查询
+     */
+    private void queryCities() {
+
+    }
+
+    private void closeProgressDialog() {
+
+    }
+
+    private void showProgressDilog() {
+
+    }
+
 }
